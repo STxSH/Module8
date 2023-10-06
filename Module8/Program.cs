170f3635dcf752daa88f5985782de4ca4305eaf3
STxSH <anton.batalov94@gmail.com>
2023-10-06 13:15:20 +0700

    Добавьте файлы проекта.

---
+++ Module8/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Module8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+
+        //task 8.1.4
+
+        public class Drive
+        {
+            public Drive(string name, long totalSpace, long freeSpace)
+            {
+                Name = name;
+                TotalSpace = totalSpace;
+                FreeSpace = freeSpace;
+            }
+
+            public string Name;
+            public long TotalSpace;
+            public long FreeSpace;
+        }
+    }
+}
