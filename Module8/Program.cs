ef19244de7516da0bac40d4d5e9c8c7ef0f7e180
Anton <anton.batalov94@gmail.com>
2023-10-08 17:13:39 +0700

    task 8.3.1 done

--- Module8/Program.cs
+++ Module8/Program.cs
@@ -5,29 +5,29 @@ namespace Module8
     internal class Program
     {
 
-        //task 8.2.4
+        //task 8.3.1
 
         static void Main(string[] args)
         {
-            string path = "D:\\232323";
-            string recyclePath = "D:\\ТипоКорзина\\232323";
-            MoveDirectory(path, recyclePath);
+            string filePath = "D:\\YandexAnton\\main\\YandexDisk\\Синхронизация\\Skillfactory\\Module8\\Module8\\Program.cs";
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine("Указанный файл не найден!");
+            }
+            else
+            {
+                using (StreamReader sr = File.OpenText(filePath))
+                {
+                    string str = "";
+                    while ((str = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(str);
+                    }
+                }
+            }
             Console.ReadKey();
         }
          
-        static void MoveDirectory(string path, string recyclePath)
-        {
-            try
-            {
-                DirectoryInfo dir = new DirectoryInfo(path);
-                string newPath = recyclePath;
-
-                if (dir.Exists && !Directory.Exists(newPath)) { dir.MoveTo(newPath); }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-        }
+        
     }
 }
