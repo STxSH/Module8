789e3cffcf94ffbd0085b02b974c0903ecc6c392
Anton <anton.batalov94@gmail.com>
2023-10-08 17:28:54 +0700

    task 8.3.2 done

--- Module8/Program.cs
+++ Module8/Program.cs
@@ -5,19 +5,24 @@ namespace Module8
     internal class Program
     {
 
-        //task 8.3.1
+        //task 8.3.2
 
         static void Main(string[] args)
         {
             string filePath = "D:\\YandexAnton\\main\\YandexDisk\\Синхронизация\\Skillfactory\\Module8\\Module8\\Program.cs";
+            FileInfo file = new FileInfo(filePath);
             if (!File.Exists(filePath))
             {
                 Console.WriteLine("Указанный файл не найден!");
             }
             else
             {
-                using (StreamReader sr = File.OpenText(filePath))
+                using (StreamWriter sw = file.AppendText())
                 {
+                    sw.WriteLine($"//Время последнего запуска: {DateTime.Now.ToString()}");
+                }
+                using (StreamReader sr = File.OpenText(filePath))
+                {                    
                     string str = "";
                     while ((str = sr.ReadLine()) != null)
                     {
@@ -26,8 +31,7 @@ namespace Module8
                 }
             }
             Console.ReadKey();
-        }
-         
-        
+        }        
     }
 }
+//Время последнего запуска: 08.10.2023 17:28:22
